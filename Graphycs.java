818ebfc5d987859b3827a67ea0738dbc9d2c9922
TommasoPa <tommaso.pattaro@hotmail.com>
2024-02-14 22:47:02 +0100

    Changed colors form RGB to 24-bit integers, added basic display control

---
+++ Graphycs.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+
+public class Graphycs {
+    public void Graphycs() {
+        
+        Frame display = new Frame();
+
+        display.setVisible(true);
+        display.setSize(800, 800);
+    }
+}
